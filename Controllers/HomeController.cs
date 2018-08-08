906800a06a0d63448cd7865f49381d82bf1b0895
fluffyasshole <rafaymodi18@gmail.com>
2018-08-08 18:32:18 +0500

    Alpha

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication14.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbentityEntities db = new dbentityEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.Tables.ToList());
+        }
+    }
+}
